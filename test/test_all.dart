43d0899b33747f1200d12a057b1e827eacab0ed2
Michael Marner <michael@20papercups.net>
2018-09-27 08:14:14 +0930

    Configuration for testing and coverage on travis

---
+++ test/test_all.dart
@@ -0,0 +1,7 @@
+import 'message_parser_test.dart' as messageParser;
+
+/// Script for running all tests on Travis CI
+/// Allows us to generate code coverage
+void main() {
+  messageParser.main();
+}
