c8084ebea6ee5554f368621051311b728e727b29
Keal Jones <keal.jones@workiva.com>
2019-10-23 13:21:33 -0700

    add export of interface outside of conditional

--- lib/src/socket_platform.dart
+++ lib/src/socket_platform.dart
@@ -1,9 +1,9 @@
 import 'socket_platform_interface.dart';
 
+export 'socket_platform_interface.dart';
+
 // Use conditional imports in order to automatically setup the platform without any user side setup.
 export 'socket_platform_interface.dart'
-  // `dart.library.js` is compatible with node and browser via dart2js -- `dart.library.html` will only work for the browser
-  // or at lest it seemed it should be, when I tried `dart.library.js` in chrome, it failed to evaluate to true
   if (dart.library.io) './socket_platform_io.dart'
   if (dart.library.js) './socket_platform_http.dart';
 
