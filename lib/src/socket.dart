62114284a14c731564bf12fdc183971bea80a71e
Michael Marner <michael@20papercups.net>
2021-03-19 18:07:33 +1030

    Support null safety (#14)
    This mostly maintains existing behavior - in that most things remain nullable and have checks for null.
    
    Future work could be to put null safety to use and clean up some of the code.

--- lib/src/socket.dart
+++ lib/src/socket.dart
@@ -10,14 +10,14 @@ import './emitter.dart';
 
 class Socket extends Emitter {
   dynamic _socket;
-  String url;
-  String id;
-  final ReconnectStrategy strategy;
-  final BasicListener listener;
+  String? url;
+  String? id;
+  final ReconnectStrategy? strategy;
+  final BasicListener? listener;
   int _counter = 0;
-  String authToken;
-  final List<Channel> channels = new List();
-  final Map<int, List<dynamic>> _acks = new Map();
+  String? authToken;
+  final List<Channel> channels = [];
+  final Map<int, List<dynamic>> _acks = {};
 
   int get state => _socket.readyState ?? CLOSED;
 
@@ -41,9 +41,9 @@ class Socket extends Emitter {
   }
 
   static Future<Socket> connect(String url,
-      {String authToken,
-      ReconnectStrategy strategy,
-      BasicListener listener}) async {
+      {String? authToken,
+      ReconnectStrategy? strategy,
+      BasicListener? listener}) async {
     if (globalSocketPlatform is IoSocketPlatform) {
       var socket = await globalSocketPlatform.webSocket(url);
       return new Socket._internal(
@@ -104,13 +104,13 @@ class Socket extends Emitter {
     dynamic json = jsonEncode(authObject);
     sendOrAdd(json);
     if (listener != null) {
-      listener.onConnected(this);
+      listener!.onConnected(this);
     }
   }
 
   void onSocketDone([event]) {
     if (listener != null) {
-      listener.onDisconnected(this);
+      listener!.onDisconnected(this);
     }
   }
 
@@ -121,7 +121,7 @@ class Socket extends Emitter {
   }
 
   void handleMessage([dynamic messageEvent]) {
-    String message;
+    String? message;
     if (globalSocketPlatform is IoSocketPlatform) {
       message = messageEvent;
     } else {
@@ -132,26 +132,26 @@ class Socket extends Emitter {
     } else {
 //      print('Message received: $message');
 
-      var map = jsonDecode(message);
+      var map = jsonDecode(message!);
       var data = map['data'];
-      int rid = map['rid'];
-      int cid = map['cid'];
-      String event = map['event'];
+      int? rid = map['rid'];
+      int? cid = map['cid'];
+      String? event = map['event'];
 
 //      print('Event: $event, rid: $rid, cid: $cid, data: $data');
 
       switch (Parser.parse(data, rid, cid, event)) {
         case ParseResult.ISAUTHENTICATED:
 //          print('IS authenticated got called');
-          id = data['id'] as String;
-          bool auth = data['isAuthenticated'] as bool;
+          id = data['id'] as String?;
+          bool? auth = data['isAuthenticated'] as bool?;
           if (listener != null) {
-            listener.onAuthentication(this, auth);
+            listener!.onAuthentication(this, auth);
           }
           subscribeChannels();
           break;
         case ParseResult.PUBLISH:
-          handlePublish(data['channel'] as String, data['data']);
+          handlePublish(data['channel'] as String?, data['data']);
 //          print('Publish got called');
           break;
         case ParseResult.REMOVETOKEN:
@@ -160,7 +160,7 @@ class Socket extends Emitter {
           break;
         case ParseResult.SETTOKEN:
           if (listener != null) {
-            listener.onSetAuthToken(data['token'] as String, this);
+            listener!.onSetAuthToken(data['token'] as String?, this);
           }
 //          print('Set token got called');
           break;
@@ -174,7 +174,7 @@ class Socket extends Emitter {
         case ParseResult.ACKRECEIVE:
 //          print('Ack receive got called');
           if (_acks.containsKey(rid)) {
-            var mapObj = _acks[rid];
+            var mapObj = _acks[rid!];
             _acks.remove(rid);
             if (mapObj != null) {
               AckCall fn = mapObj[1];
@@ -192,19 +192,19 @@ class Socket extends Emitter {
     }
   }
 
-  AckCall ack(int cid) {
+  AckCall ack(int? cid) {
     return (name, dynamic error, dynamic data) {
       var message = {
-        'error': error as String,
-        'data': data as String,
-        'rid': cid as String, // FIXME: rid -> cid?
+        'error': error as String?,
+        'data': data as String?,
+        'rid': cid as String?, // FIXME: rid -> cid?
       };
       var json = jsonEncode(message);
       sendOrAdd(json);
     };
   }
 
-  Socket emit(String event, Object data, [AckCall ack]) {
+  Socket emit(String event, Object data, [AckCall? ack]) {
     int count = ++_counter;
     var message = new Map<String, Object>();
     message['event'] = event;
@@ -219,7 +219,7 @@ class Socket extends Emitter {
     return this;
   }
 
-  Socket subscribe(String channel, [AckCall ack]) {
+  Socket subscribe(String channel, [AckCall? ack]) {
     int count = ++_counter;
     var message = {
       'event': '#subscribe',
@@ -232,7 +232,7 @@ class Socket extends Emitter {
     return this;
   }
 
-  Socket unsubscribe(String channel, [AckCall ack]) {
+  Socket unsubscribe(String channel, [AckCall? ack]) {
     int count = ++_counter;
     var message = {'event': '#unsubscribe', 'data': channel, 'cid': count};
     if (ack != null) _acks[count] = getAckObject(channel, ack);
@@ -241,7 +241,7 @@ class Socket extends Emitter {
     return this;
   }
 
-  Socket publish(String channel, Object data, [AckCall ack]) {
+  Socket publish(String channel, Object data, [AckCall? ack]) {
     int count = ++_counter;
     var message = {
       'event': '#publish',
